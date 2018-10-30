40466e373a2f6b4a4071ad533b230a0316da49f5
Luis Fernando Raga Renteria <whary11@gmail.com>
2018-10-30 17:25:11 -0500

    Haciendo las configuraciones iniciales del proyecto..

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,19 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rutas web
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aquí es donde puede registrar rutas web para su aplicación. Estas
+| RouteServiceProvider carga las rutas dentro de un grupo que
+| Contiene el grupo de middleware "web". Ahora crea algo genial!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
